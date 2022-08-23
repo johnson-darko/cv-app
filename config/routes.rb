ba38d01dd70a9aa24761824ca3990b501d0a6f36
johnson-darko <johnsondarko365@gmail.com>
2022-08-23 21:56:13 +0200

    added initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+resources :resumes do
+  resources :educations
 
+end
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "home#index"
+  root 'resumes#index'
 end
